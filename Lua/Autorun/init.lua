a09c466ccd576ad8a4c544fa87b5daad1c2443f4
Ydrec <53411693+Ydrec@users.noreply.github.com>
2024-07-26 17:48:04 +0300

    Update version in init.lua

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,6 +1,6 @@
 NTID = {}
 NTID.Name="NT Informative Descriptions"
-NTID.Version = "1.1"
+NTID.Version = "1.2"
 NTID.Path = table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTID) end end,1)
 

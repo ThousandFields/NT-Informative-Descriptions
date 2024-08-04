0199007dac05b365aa3f1292f5f27732cf51f696
Ydrec <53411693+Ydrec@users.noreply.github.com>
2024-08-05 00:15:58 +0300

    Dont need dummy in config

--- Lua/modconfig.lua
+++ Lua/modconfig.lua
@@ -41,17 +41,17 @@ local config = {
 		files = {
 			"%ModDir%/Localization/%Language%/Cybernetics.xml"
 			} 
-	},
-	-- Example blank
-	{
-		supportedlanguages = {"English"},
-		IgnoreTargetModState = false,
-		workshopId = "", -- if empty doesnt announce patch being enabled or check for other mods 
-		loadpriority = 1, -- patches with same priority would load in random order
-		files = {
-			"%ModDir%/Localization/%Language%/dummy.xml"
-			} 
 	}
+	-- Example blank, you dont need to add localization for this
+	-- {
+	-- 	supportedlanguages = {"English"},
+	-- 	IgnoreTargetModState = false,
+	-- 	workshopId = "", -- if empty doesnt announce patch being enabled or check for other mods 
+	-- 	loadpriority = 1, -- patches with same priority would load in random order
+	-- 	files = {
+	-- 		"%ModDir%/Localization/%Language%/dummy.xml"
+	-- 		} 
+	-- }
 }
 
 return config

34b477c7e2a0f7dbd85adb0e1249ae1c7a916775
Ydrec <53411693+Ydrec@users.noreply.github.com>
2024-08-16 16:55:12 +0300

    Fuck timers i guess
    Fixes subeditor crash but potentially makes mod not detect NT being loaded

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -15,7 +15,7 @@ NTworkshopIds = {
 function IsNTEnabled()
     for package in ContentPackageManager.EnabledPackages.All do
         for NTworkshopId in NTworkshopIds do
-            if tostring(package.UgcId) == workshopId then
+            if tostring(package.UgcId) == NTworkshopId then
                 return true
             end
         end
@@ -24,23 +24,38 @@ function IsNTEnabled()
 end
 
 
+-- if CLIENT or not Game.IsMultiplayer then
+--     Timer.Wait(function()
+--         if NTC ~= nil or NT ~= nil or IsNTEnabled() then
+--             dofile(NTID.Path .. '/Lua/main.lua')
+--             return
+--         end
+--         print("Error loading NT Informative Descriptions: it appears that Neurotrauma isn't enabled!")
+--   end,1)
+-- end
+
+
+-- if CLIENT and Game.IsSubEditor then
+--   Timer.Wait(function()
+--     if NTC ~= nil or NT ~= nil or IsNTEnabled() then
+--         LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.SubEditorScreen"], "UpdateEntityList")
+--         Game.SubEditorScreen.UpdateEntityList()
+--         return
+--     end
+--   end,1000)
+-- end
+
+
 if CLIENT or not Game.IsMultiplayer then
-    Timer.Wait(function()
-        if NTC ~= nil or NT ~= nil or IsNTEnabled() then
-            dofile(NTID.Path .. '/Lua/main.lua')
-            return
+    if NTC ~= nil or NT ~= nil or IsNTEnabled() then
+        dofile(NTID.Path .. '/Lua/main.lua')
+        if Game.IsSubEditor then
+            LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.SubEditorScreen"], "UpdateEntityList")
+            Game.SubEditorScreen.UpdateEntityList()
         end
-        print("Error loading NT Informative Descriptions: it appears that Neurotrauma isn't enabled!")
-  end,1)
-end
-
-
-if CLIENT and Game.IsSubEditor then
-  Timer.Wait(function()
-	if NTC ~= nil or NT ~= nil or IsNTEnabled() then
-        LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.SubEditorScreen"], "UpdateEntityList")
-        Game.SubEditorScreen.UpdateEntityList()
         return
-	end
-  end,5)
+    end
+    Timer.Wait(function()
+        print("Error loading NT Informative Descriptions: it appears Neurotrauma isn't loaded!")
+    end,1)
 end

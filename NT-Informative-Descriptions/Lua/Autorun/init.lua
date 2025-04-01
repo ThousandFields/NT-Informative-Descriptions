484bd565a081a28ef5510b669e779f99f8782cfd
ThousandFields <mattthejayfeatherfan@gmail.com>
2025-04-01 00:02:23 -0400

    increment version to 1.16

--- NT-Informative-Descriptions/Lua/Autorun/init.lua
+++ NT-Informative-Descriptions/Lua/Autorun/init.lua
@@ -1,6 +1,6 @@
 NTID = {}
 NTID.Name="Informative Descriptions"
-NTID.Version = "1.15"
+NTID.Version = "1.16"
 NTID.Path = table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTID) end end,1)
 
